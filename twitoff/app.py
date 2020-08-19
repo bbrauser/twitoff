d7b65ee0f5bf04a8db3cf8e548e63dffa16aa348
bbrauser <65994299+bbrauser@users.noreply.github.com>
2020-08-19 13:19:36 -0500

    Added ability to make predictions about tweets

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,8 @@
 """Main app/routing file for TwitOff."""
-from flask import Flask, render_template
-from .models import DB, User, insert_example_users, insert_example_tweets
+from flask import Flask, render_template, request
+from .models import DB, User
+from .predict import predict_user
+from .twitter import add_or_update_user, insert_example_users
 
 
 def create_app():
@@ -14,18 +16,49 @@ def create_app():
     @app.route('/')
     def root():
         return render_template('base.html', title='Home',
-                               users=User.query.all(),
-                               tweets=Tweet.query.all())
+                               users=User.query.all())
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                               message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2,
+                                      request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction',
+                               message=message)
 
     @app.route('/update')
     def update():
         # Reset the database
-        DB.drop_all()
-        DB.create_all()
-        insert_example_users()
-        insert_example_tweets()
+        insert_example_users()  # Optional - update existing users
         return render_template('base.html', title='Users updated!',
                                users=User.query.all())
-       
-        
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset database!')
+
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -8,6 +8,8 @@ class User(DB.Model):
     """Twitter users corresponding to Tweets."""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    # Tweet IDs are ordinal ints, so can be used to fetch only more recent
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return '-User {}-'.format(self.name)
@@ -17,36 +19,9 @@ class Tweet(DB.Model):
     """Tweet text and data."""
     id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(300))  # Allows for text + links
+    embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
         return '-Tweet {}-'.format(self.text)
-
-
-def insert_example_users():
-    """Example data to play with."""
-    austen = User(id=1, name='austen')
-    elon = User(id=2, name='elonmusk')
-    bbrauser = User(id=3, name='bbrauser')
-    DB.session.add(austen)
-    DB.session.add(elon)
-    DB.session.add(bbrauser)
-    DB.session.commit()
-
-
-def insert_example_tweets():
-    """Example tweets"""
-    t1 = Tweet(id=1, text='This is a tweet', user_id=3)
-    t2 = Tweet(id=2, text='No, THIS is a tweet', user_id=1)
-    t3 = Tweet(id=3, text='You are both wrong...THIS is a tweet!', user_id=2)
-    t4 = Tweet(id=4, text='Why are we arguing?', user_id=3)
-    t5 = Tweet(id=5, text='You are on Twitter, that is what you do', user_id=2)
-    t6 = Tweet(id=6, text='Word', user_id=1)
-    DB.session.add(t1)
-    DB.session.add(t2)
-    DB.session.add(t3)
-    DB.session.add(t4)
-    DB.session.add(t5)
-    DB.session.add(t6)
-    DB.session.commit()
